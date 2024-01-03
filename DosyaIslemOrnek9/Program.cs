a211ec9333aa2ce38913e4631d055aee38679871
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2024-01-03 11:07:55 +0300

    Proje dosyası ekle.

---
+++ DosyaIslemOrnek9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DosyaIslemOrnek9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Belirtilen klasördeki tüm dosyaları sil
+            
+            //String[] liste = Directory.GetFiles("c:\\users\\omer\\desktop\\deneme");
+            String masaustu= Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            String[] liste = Directory.GetFiles(masaustu + "\\deneme");
+            //belirtilen dizideki dosyaların listesini ver
+            for (int i = 0; i < liste.Length; i++)
+            {
+                File.Delete(liste[i]);
+            }
+        }
+    }
+}
